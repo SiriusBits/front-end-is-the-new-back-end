c5916a6ac2891abf9f9f518be741c1a78600458e
Benjamin Bykowski <benjamin.bykowski@gmail.com>
2018-05-04 00:47:15 -0400

    Ready for talk

--- test-speed/my-app/src/appdata/index.js
+++
@@ -1,9 +0,0 @@
-import { coinState, coinActions } from './_main.js';
-
-export const appData = {
-  ...coinState,
-};
-
-export const actions = {
-  ...coinActions,
-};
--- test-speed/my-app/src/components/helpers/utilities.js
+++
@@ -1,9 +0,0 @@
-/**
- * @desc format price decimals and thousands separators
- * @param  {String} price
- * @return {String}
- */
-export const formatPrice = price => {
-  const number = Number(price).toFixed(2);
-  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-};
